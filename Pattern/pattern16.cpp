53d4a9d9367b84383e70c2a113546398df0cafa8
Jenil Shingala <jenilshingala1002@gmail.com>
2026-06-30 03:00:14 -0700

    Pattern 16

---
+++ Pattern/pattern16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void pattern(int n) {
+    char ch= 'A';
+    for (int i =0; i  <=n; i++){
+        for (int j= 0; j<= i; j++)cout <<ch;
+        ch++;
+        cout<<endl;
+}}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
