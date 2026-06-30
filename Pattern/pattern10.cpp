f3f210681e11b7958a615efc7164a6a7890e07b4
Jenil Shingala <jenilshingala1002@gmail.com>
2026-06-30 00:40:43 -0700

    pattern 6

---
+++ Pattern/pattern10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void pattern(int n) {
+    for(int i = 0; i <n; i++){
+        for(int j = 0; j<=i;j++){
+            cout << "*";
+        }cout << "\n";
+    }
+    for(int i = 0; i <n-1; i++){
+        for(int j = 0; j< n-i -1;j++){
+            cout <<"*";
+        }cout << endl;
+    }
+}
+        
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
