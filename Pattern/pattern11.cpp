e1410e7cfa4eee95370640c0726be4f5f42298d5
Jenil Shingala <jenilshingala1002@gmail.com>
2026-06-30 01:45:25 -0700

    Pattern11

---
+++ Pattern/pattern11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void pattern(int n) {
+    for(int i= 0; i < n;i++){
+        for(int j= 0; j<=i;j++){
+            if (j%2==0){
+                cout<<"1";
+            }
+            else{
+                cout<<"0";
+            }
+        }cout <<"\n";
+    }
+}
+        
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
