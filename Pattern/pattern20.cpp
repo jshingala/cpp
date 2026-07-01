bea06a23de449926077e1c8181da55e6e8788675
Jenil Shingala <jenilshingala1002@gmail.com>
2026-06-30 23:46:54 -0700

    Pattern 20

--- Pattern/pattern20.cpp
+++ Pattern/pattern20.cpp
@@ -12,7 +12,7 @@ void pattern(int n) {
     for(int i=1; i<=n;i++){
         for (int j=0;j<n-i;j++)cout<<"*";
         for(int k=0; k<2*i;k++)cout<<" ";
-        for(int l=0; l<n-1;l++)cout<<"*";
+        for(int l=0; l<n-i;l++)cout<<"*";
         cout<<"\n";
     }
 
